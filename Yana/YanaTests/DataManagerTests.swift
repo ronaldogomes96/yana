69b250d70237b6dec7cb9268f02efafac7ccb91b
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-07-10 02:12:55 -0300

    Criacao das classes bases de testes unitarios

---
+++ Yana/YanaTests/DataManagerTests.swift
@@ -0,0 +1,15 @@
+//
+//  DataManagerTests.swift
+//  YanaTests
+//
+//  Created by Ronaldo Gomes on 10/07/20.
+//  Copyright © 2020 Pedro Sousa. All rights reserved.
+//
+
+import XCTest
+
+@testable import Yana
+
+class DataManagerTests: XCTestCase {
+
+}
---
+++ Yana/YanaTests/PresentationContentTests.swift
@@ -0,0 +1,15 @@
+//
+//  PresentationContentTests.swift
+//  YanaTests
+//
+//  Created by Ronaldo Gomes on 10/07/20.
+//  Copyright © 2020 Pedro Sousa. All rights reserved.
+//
+
+import XCTest
+
+@testable import Yana
+
+class PresentationContentTests: XCTestCase {
+
+}
